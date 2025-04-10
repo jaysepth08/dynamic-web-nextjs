00dc2f5f4ba61ba849f391f69e63a2f424c2532d
lordelements <andrianelemento41@gmail.com>
2025-04-10 13:59:22 +0800

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    allowedDevOrigins: ['http://192.168.100.115:3000'], // Replace with your actual IP and port
+  },
 };
 
 export default nextConfig;
