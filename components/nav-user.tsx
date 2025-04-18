091a7675764d95df6d61fb8f3d707cbf450fc137
elementovivianjoy <elementoandrian11@gmail.com>
2025-04-18 10:22:54 +0800

    Hello

--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -3,11 +3,8 @@
 import { useRouter } from "next/navigation"
 import {
   BadgeCheck,
-  Bell,
   ChevronsUpDown,
-  CreditCard,
   LogOut,
-  Sparkles,
 } from "lucide-react"
 
 import {
@@ -40,107 +37,6 @@ type NavUserProps = {
   }
 }
 
-// export function NavUser({
-//   user,
-// }: {
-//   user: {
-//     name: string
-//     email: string
-//     avatar: string
-//   }
-// }) {
-//   // const { isMobile } = useSidebar()
-
-//   const { isMobile } = useSidebar()
-//   const router = useRouter()
-
-//   const getInitials = (name: string) => {
-//     const names = name.split(" ")
-//     return names.map(n => n[0]).join("").toUpperCase()
-//   }
-
-//   const handleLogout = () => {
-//     localStorage.removeItem("user")
-//     router.push("/login") // Adjust path as needed
-//   }
-
-
-//   return (
-//     <SidebarMenu>
-//       <SidebarMenuItem>
-//         <DropdownMenu>
-//           <DropdownMenuTrigger asChild>
-//             <SidebarMenuButton
-//               size="lg"
-//               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-//             >
-//               <Avatar className="h-8 w-8 rounded-lg">
-//                 {/* <AvatarImage src={user.avatar} alt={user.name} /> */}
-//                 <AvatarImage src={user.avatar ?? "/default-avatar.png"} alt={user.name} />
-//                 <AvatarFallback className="rounded-lg">{getInitials(user.name)}</AvatarFallback>
-//                 {/* <AvatarFallback className="rounded-lg">CN</AvatarFallback> */}
-//               </Avatar>
-//               <div className="grid flex-1 text-left text-sm leading-tight">
-//                 <span className="truncate font-medium">{user.name}</span>
-//                 <span className="truncate text-xs">{user.email}</span>
-//               </div>
-//               <ChevronsUpDown className="ml-auto size-4" />
-//             </SidebarMenuButton>
-//           </DropdownMenuTrigger>
-//           <DropdownMenuContent
-//             className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
-//             side={isMobile ? "bottom" : "right"}
-//             align="end"
-//             sideOffset={4}
-//           >
-//             <DropdownMenuLabel className="p-0 font-normal">
-//               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-//                 <Avatar className="h-8 w-8 rounded-lg">
-//                   {/* <AvatarImage src={user.avatar} alt={user.name} />
-//                   <AvatarFallback className="rounded-lg">CN</AvatarFallback> */}
-//                   <AvatarImage src={user.avatar ?? "/default-avatar.png"} alt={user.name} />
-//                   <AvatarFallback className="rounded-lg">{getInitials(user.name)}</AvatarFallback>
-//                 </Avatar>
-//                 <div className="grid flex-1 text-left text-sm leading-tight">
-//                   <span className="truncate font-medium">{user.name}</span>
-//                   <span className="truncate text-xs">{user.email}</span>
-//                 </div>
-//               </div>
-//             </DropdownMenuLabel>
-//             <DropdownMenuSeparator />
-//             <DropdownMenuGroup>
-//               <DropdownMenuItem>
-//                 <Sparkles />
-//                 Upgrade to Pro
-//               </DropdownMenuItem>
-//             </DropdownMenuGroup>
-//             <DropdownMenuSeparator />
-//             <DropdownMenuGroup>
-//               <DropdownMenuItem>
-//                 <BadgeCheck />
-//                 Account
-//               </DropdownMenuItem>
-//               <DropdownMenuItem>
-//                 <CreditCard />
-//                 Billing
-//               </DropdownMenuItem>
-//               <DropdownMenuItem>
-//                 <Bell />
-//                 Notifications
-//               </DropdownMenuItem>
-//             </DropdownMenuGroup>
-//             <DropdownMenuSeparator />
-//             <DropdownMenuItem onClick={handleLogout}>
-//               <LogOut />
-//               Log out
-//             </DropdownMenuItem>
-//           </DropdownMenuContent>
-//         </DropdownMenu>
-//       </SidebarMenuItem>
-//     </SidebarMenu>
-//   )
-
-// }
 
 export function NavUser({ user }: NavUserProps) {
   const { isMobile } = useSidebar()
@@ -197,24 +93,10 @@ export function NavUser({ user }: NavUserProps) {
             <DropdownMenuSeparator />
             <DropdownMenuGroup>
               <DropdownMenuItem>
-                <Sparkles />
-                Upgrade to Pro
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem>
                 <BadgeCheck />
                 Account
               </DropdownMenuItem>
-              <DropdownMenuItem>
-                <CreditCard />
-                Billing
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Bell />
-                Notifications
-              </DropdownMenuItem>
+             
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
             <DropdownMenuItem onClick={handleLogout}>
