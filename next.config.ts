677007a615115700e9b9c4027de7f95ad8e859a7
jaysepth08 <jayarpableo0@gmail.com>
2025-05-15 14:04:34 +0800

    new commit....

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
 // const nextConfig: NextConfig = {
 //   /* config options here */
@@ -10,12 +10,71 @@
 // export default nextConfig;
 
 
-module.exports = {
+// module.exports = {
+//   images: {
+//     domains: ['robohash.org'], 
+//   },
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+// };
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
-    domains: ['robohash.org'],  // Add 'robohash.org' here
+    domains: ['robohash.org'],
   },
   eslint: {
-    // Disable ESLint during the build process
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    appDir: true,
+    optimizeCss: false,
+    serverActions: true,
+    serverComponentsExternalPackages: [],
+  },
+  webpack: (config: import('webpack').Configuration) => {
+    config.resolve = config.resolve ?? {};
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname),
+    };
+    return config;
+  },
+
+  optimizeFonts: true,
+  compress: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
+  trailingSlash: false,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+        ],
+      },
+    ];
+  },
 };
+
+module.exports = nextConfig;
